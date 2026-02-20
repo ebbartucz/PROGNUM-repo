558dec2ba7f2dae5c063f341e7e2b52b96bae0c1
E.B. Bartucz <ebbartucz@hydra26.desktop.astro.rug.nl>
2026-02-20 10:08:21 +0100

    week 2 update

---
+++ Task1/myfirst.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Answers for Week 1
+# 
+# * Name: Eszter Bartucz
+# * Username: ebbartucz
+# * Student s-number: S6362311
+# * Group (AS1, etc.): AS4
+
+# Question 1.2:
+
+# In[3]:
+
+
+x=22/7
+
+
+# $$y=\frac{sin(x)}{x}$$
+
+# In[1]:
+
+
+pi=22/7 #Processed on Jupyter Hub
+
+
+# Question 1.5:
+
+# In[7]:
+
+
+import math
+from scipy.constants import G
+
+m_moon=7.342*10**22
+m_earth=5.9722*10**24
+d=385000.6
+
+
+# In[13]:
+
+
+F=(G*m_moon*m_earth)/(d*1000)**2
+print("The gravitational pull is", F ,"N.")
+
+
+# Question 1.15:
+
+# 1. The command pwd will print the path to Downloads thus /home/localuser3/Downloads.
+# 2. The notation . refers to the current directory and .. refers to the parent directory.
+
+# Question 1.16:
+
+# cd ~/Desktop (cd ../Desktop)
+
+# Question 1.10:
+
+# I will download this notebook as a .py file.
